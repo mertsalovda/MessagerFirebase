a86a06cd3608fb4fcc0d9eba42b484a0334a28d0
Dmitriy Mertsalov <korranhorn009@gmail.com>
2020-04-03 07:52:32 +0300

    Fix DiffUtils

--- app/src/main/java/ru/mertsalovda/firebase/utils/MessagesDiffUtilsCallback.kt
+++ app/src/main/java/ru/mertsalovda/firebase/utils/MessagesDiffUtilsCallback.kt
@@ -3,19 +3,21 @@ package ru.mertsalovda.firebase.utils
 import androidx.recyclerview.widget.DiffUtil
 import ru.mertsalovda.firebase.model.Message
 
-internal class MessagesDiffUtilsCallback(private val oldList: List<Message>,
-                                         private val newList: List<Message>
+internal class MessagesDiffUtilsCallback(
+    private val oldList: List<Message>,
+    private val newList: List<Message>
 ) :
     DiffUtil.Callback() {
 
     override fun getOldListSize(): Int = oldList.size
 
-    override fun getNewListSize(): Int  = newList.size
+    override fun getNewListSize(): Int = newList.size
 
     override fun areItemsTheSame(oldItemPosition: Int, newItemPosition: Int): Boolean {
         val oldMessage = oldList[oldItemPosition]
         val newMessage = newList[newItemPosition]
         return oldMessage.name == newMessage.name
+                && oldMessage.date == newMessage.date
     }
 
     override fun areContentsTheSame(oldItemPosition: Int, newItemPosition: Int): Boolean {
@@ -24,6 +26,7 @@ internal class MessagesDiffUtilsCallback(private val oldList: List<Message>,
         return oldMessage.name == newMessage.name
                 && oldMessage.date == newMessage.date
                 && oldMessage.text == newMessage.text
+                && oldMessage.photoUrl == newMessage.photoUrl
 
     }
 }
